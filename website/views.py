eeb176c90f7f0b6f72b4353ff6894ad034a4fc1c
Priya-173github <paripriya173@gmail.com>
2025-04-12 12:02:23 +0530

    Views.py page update

--- website/views.py
+++ website/views.py
@@ -4,37 +4,49 @@ from . import Scraper
 from . import API
 import logging
 import re
+from .models import SearchedQuery
+from datetime import datetime
+from . import db
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/feed', methods=['GET'])
-def feed():
-    return render_template('socialmedia.html', user=current_user)
 
-@views.route("/search", methods=['GET', 'POST'])
+@views.route("/feed", methods=["GET"])
+def feed():
+    return render_template("socialmedia.html", user=current_user)
+
+
+@views.route("/search", methods=["GET", "POST"])
 def search():
-    if request.method == 'POST':
-        query = request.form['query']
+    if request.method == "POST":
+        query = request.form["query"]
         abstracts = Scraper.fetch_arxiv_abstracts(query)
         if abstracts:
             return render_template("results.html", abstracts=abstracts)
         else:
             return "No results found", 404
-    return render_template('search.html')
+    return render_template("search.html")
 
-@views.route('/grants')
+
+@views.route("/grants")
 def grants():
-    return render_template('grants.html')
+    return render_template("grants.html")
 
-@views.route('/process', methods=['POST'])
+
+@views.route("/history")
+def history():
+    return render_template("history.html")
+
+
+@views.route("/process", methods=["POST"])
 def process_abstracts():
     try:
-        query = request.form['query']
+        query = request.form["query"]
         abstracts = Scraper.fetch_arxiv_abstracts(query)
         if not abstracts:
             return "No abstracts found to process.", 404
@@ -51,10 +63,11 @@ def process_abstracts():
         logging.error(f"Error during processing: {str(e)}")
         return str(e), 500
 
-@views.route('/qa', methods=['POST'])
+
+@views.route("/qa", methods=["POST"])
 def ask_summary_question():
-    context = request.form.get('context')
-    question = request.form.get('question')
+    context = request.form.get("context")
+    question = request.form.get("question")
 
     if not context or not question:
         return "Missing context or question", 400
@@ -65,6 +78,7 @@ def ask_summary_question():
     answer = API.summarize_text(model, prompt)
 
     return render_template("AI.html", summaries=[context], answer=answer)
+
 
 def process_summary_text(summary):
     summary = re.sub(r"\*\*(.*?)\*\*", r"<strong>\1</strong>", summary)
